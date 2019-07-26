9236a03eec189f636be851916c6eee7f954f5296
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2019-07-26 09:30:58 +0200

    only fix initial tether bounds for lift-mode power cycles

--- awebox/opti/preparation.py
+++ awebox/opti/preparation.py
@@ -168,7 +168,8 @@ def set_initial_bounds(nlp, model, formulation, options, V_init):
                 V_bounds['ub']['coll_var', :, :, 'u', name] = cas.inf
 
     # set state bounds
-    if (options['initialization']['type'] == 'lift_mode') or (options['initialization']['type'] == 'tracking'):
+    if (options['initialization']['type'] == 'power_cycle' and options['initialization']['system_type'] == 'lift_mode') \
+        or (options['initialization']['type'] == 'tracking'):
         if 'ddl_t' in list(model.variables_dict['u'].keys()):
             if 'u' in V_init.keys():
                 V_bounds['lb']['u', :, 'ddl_t'] = 0.
