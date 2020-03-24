deb675df91488f64048425dafbd76d65d9708e16
rcleuthold <rcleuthold@gmail.com>
2020-03-24 10:10:41 +0100

    a_i_h again

--- awebox/opti/preparation.py
+++ awebox/opti/preparation.py
@@ -265,7 +265,8 @@ def generate_solvers(awebox_callback, model, nlp, formulation, options):
 
     default_opts = generate_default_solver_options(options)
     default_opts['ipopt.mu_target'] = options['mu_hippo']
-    default_opts['ipopt.acceptable_iter'] = 10 # options['acceptable_iter_hippo']  # 5
+    print(options['acceptable_iter_hippo'])
+    default_opts['ipopt.acceptable_iter'] = 5 # options['acceptable_iter_hippo']  # 5
     default_sol = cas.nlpsol('solver', 'ipopt', nlp.get_nlp(), default_opts)
 
     pdb.set_trace()
