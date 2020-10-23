DOES NOT RUN. attempt to fix floating-point-licq due to large tether force derivatives

awebox/ocp/collocation.py
@@ -175,7 +175,7 @@ class Collocation(object):
 
         # compute quadrature weights
         Lambda = np.linalg.solve(coeff_collocation[1:,1:], np.eye(len(coeff_collocation)-1))
-        quad_weights_1 = np.matmul(Lambda,coeff_continuity[1:])
+        quad_weights_1 = np.matmul(Lambda, coeff_continuity[1:])
         quad_weights = np.linalg.solve(coeff_collocation[1:,1:], coeff_continuity[1:])
 
         self.__quad_weights = quad_weights

awebox/opts/default.py
@@ -190,7 +190,7 @@ def set_default_options(default_user_options, help_options):
         ('model',   'model_bounds', 'tether_force',  'include',              False,     ('include tether force inequality in constraints', [True, False]),'x'),
         ('params',  'model_bounds',  None,           'tether_force_limits',  np.array([1e0, 2e3]),  ('tether force limits [N]', None),'s'),
         ('model',   'model_bounds', 'airspeed',      'include',             False,      ('include airspeed inequality for kites in constraints', [True, False]),'x'),
-        ('params',  'model_bounds',  None,           'airspeed_limits',     np.array([1.,150.]),  ('airspeed limits [m/s]', None),'s'),
+        ('params',  'model_bounds',  None,           'airspeed_limits',     np.array([1., 150.]),  ('airspeed limits [m/s]', None),'s'),
         ('model',   'model_bounds', 'aero_validity', 'include',              True,       ('include orientation bounds on alpha and beta (not possible in 3dof mode)', [True, False]),'x'),
         ('model',   'model_bounds', 'aero_validity', 'scaling',              1.,         ('tightness scaling for aero_validity inequalities', None),'x'),
         ('model',   'model_bounds', 'anticollision', 'safety_factor',        5.,         ('safety margin for anticollision constraint [m]', None),'x'),

awebox/opts/model_funcs.py
@@ -728,11 +728,8 @@ def build_wound_tether_length_options(options, options_tree, fixed_params):
     if not use_wound_tether:
         options['model']['model_bounds']['wound_tether_length']['include'] = False
 
-    l_t_scaling = options['model']['scaling']['xd']['l_t']
-    l_s_scaling = options['model']['scaling']['theta']['l_s']
-    # q_scaling = l_s_scaling
+    # todo: figure out how scaled q's will alter the tether tension.
     q_scaling = 1.
-    print_op.warn_about_temporary_funcationality_removal(location='model_funcs.q')
     options_tree.append(('model', 'scaling', 'xd', 'q', q_scaling, ('descript', None), 'x'))
 
     return options_tree, fixed_params
@@ -844,15 +841,6 @@ def build_fict_scaling_options(options, options_tree, fixed_params):
 
     options_tree.append(('model', 'scaling', 'xl', 'f_aero', f_fict_scaling, ('scaling of aerodynamic forces', None),'x'))
     options_tree.append(('model', 'scaling', 'xl', 'm_aero', m_fict_scaling, ('scaling of aerodynamic forces', None),'x'))
-
-    u_ref = get_u_ref(options['user_options'])
-    induction_varrho_ref = options['model']['aero']['actuator']['varrho_ref']
-    # t_char_flow = induction_varrho_ref * b_ref / u_ref
-    t_char_pendulum = np.sqrt(options['model']['scaling']['xd']['l_t'] / options['model']['scaling']['other']['g'])
-    # t_char = t_char_pendulum
-    t_char = 1.
-    print_op.warn_about_temporary_funcationality_removal(location='model_funcs.t')
-    options_tree.append(('model', 'scaling', 'other', 't_characteristic', t_char, ('characteristic time', None),'x'))
 
     return options_tree, fixed_params
 