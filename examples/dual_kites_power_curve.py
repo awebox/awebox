UNTESTED. fix inconsistent scaling of theta bounds

examples/dual_kites_power_curve.py
@@ -4,6 +4,8 @@ import awebox as awe
 import matplotlib.pyplot as plt
 import numpy as np
 
+from awebox.logger.logger import Logger as awelogger
+awelogger.logger.setLevel(10)
 
 ########################
 # SET-UP TRIAL OPTIONS #
@@ -14,12 +16,12 @@ options = awe.Options()
 
 # single kite with point-mass model
 options['user_options']['system_model']['architecture'] = {1:0, 2:1, 3:1}
-options['user_options']['system_model']['kite_dof'] = 6
+options['user_options']['system_model']['kite_dof'] = 3
 options['user_options']['kite_standard'] = awe.ampyx_data.data_dict()
-# trajectory should be a single pumping cycle with initial number of five windings
+# trajectory should be a single pumping cycle with initial number of three windings
 options['user_options']['trajectory']['type'] = 'power_cycle'
 options['user_options']['trajectory']['system_type'] = 'lift_mode'
-options['user_options']['trajectory']['lift_mode']['windings'] = 5
+options['user_options']['trajectory']['lift_mode']['windings'] = 2
 
 # don't include induction effects, use simple tether drag
 options['user_options']['wind']['u_ref'] = 5.0 # m/s
@@ -33,7 +35,7 @@ trial.optimize()
 # fix params for wind speed sweep
 fixed_params = {}
 for name in list(trial.model.variables_dict['theta'].keys()):
-    if name != 't_f':
+    if ('diam' in name) or (name == 'l_s'):
         fixed_params[name] = trial.optimization.V_final['theta',name].full()
 
 options['user_options']['trajectory']['fixed_params'] = fixed_params
@@ -50,5 +52,5 @@ sweep_opts = [(['user_options', 'wind', 'u_ref'], np.linspace(3,15,5, endpoint=T
 sweep = awe.Sweep(name = 'dual_kites_power_curve', options = options, seed = sweep_opts)
 sweep.build()
 sweep.run()
-sweep.plot('comp_stats')
+sweep.plot('comp_stats', 'comp_convergence')
 plt.show()