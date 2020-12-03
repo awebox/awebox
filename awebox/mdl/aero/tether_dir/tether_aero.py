e931fd8165ca8384ef239461d2c80b54578b6fcf
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2020-12-03 12:08:27 +0100

    update copyright dates

--- awebox/mdl/aero/tether_dir/tether_aero.py
+++ awebox/mdl/aero/tether_dir/tether_aero.py
@@ -2,9 +2,9 @@
 #    This file is part of awebox.
 #
 #    awebox -- A modeling and optimization framework for multi-kite AWE systems.
-#    Copyright (C) 2017-2019 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
+#    Copyright (C) 2017-2020 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
 #                            ALU Freiburg.
-#    Copyright (C) 2018-2019 Thilo Bronnenmeyer, Kiteswarms Ltd.
+#    Copyright (C) 2018-2020 Thilo Bronnenmeyer, Kiteswarms Ltd.
 #    Copyright (C) 2016      Elena Malz, Sebastien Gros, Chalmers UT.
 #
 #    awebox is free software; you can redistribute it and/or
