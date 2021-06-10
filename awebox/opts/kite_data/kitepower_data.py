e96bdfebc79044facb88ee5b50f7c39d401c2b52
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2021-06-10 20:25:52 +0200

    add kitepower data

---
+++ awebox/opts/kite_data/kitepower_data.py
@@ -0,0 +1,88 @@
+#
+#    This file is part of awebox.
+#
+#    awebox -- A modeling and optimization framework for multi-kite AWE systems.
+#    Copyright (C) 2017-2020 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
+#                            ALU Freiburg.
+#    Copyright (C) 2018-2020 Thilo Bronnenmeyer, Kiteswarms Ltd.
+#    Copyright (C) 2016      Elena Malz, Sebastien Gros, Chalmers UT.
+#
+#    awebox is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    awebox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with awebox; if not, write to the Free Software Foundation,
+#    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+import numpy as np
+from casadi.tools import vertcat
+
+def data_dict():
+
+    data_dict = {}
+    data_dict['name'] = 'kitepower'
+
+    data_dict['geometry'] = geometry() # kite geometry
+
+    stab_derivs, aero_validity = aero()
+    data_dict['stab_derivs'] = stab_derivs # stability derivatives
+    data_dict['aero_validity'] = aero_validity
+
+    return data_dict
+
+def geometry():
+
+    geometry = {}
+    geometry['m_k'] = 36.8  # [kg]
+    geometry['s_ref'] = 200.0 # [m^2]
+    geometry['s_ref_side'] = 10.0 # [m^2]
+
+    # tether attachment point
+    geometry['r_tether'] = np.zeros((3,1)) # assumed at COM
+
+    geometry['b_ref'] = 1.0  # [m]
+    geometry['c_ref'] = geometry['s_ref'] / geometry['b_ref']  # [m]
+
+    return geometry
+
+def aero():
+    # commented values are not currently supported, future implementation
+
+    stab_derivs = {}
+
+    stab_derivs['frame'] = {}
+    stab_derivs['frame']['force'] = 'control'
+    stab_derivs['frame']['moment'] = 'control'
+
+    stab_derivs['CX'] = {}
+    stab_derivs['CX']['0'] = [-0.0293]
+    stab_derivs['CX']['alpha'] = [0.4784, 2.5549]
+
+    stab_derivs['CY'] = {}
+    stab_derivs['CY']['0'] = [-0.5526]
+
+    stab_derivs['CZ'] = {}
+    stab_derivs['CZ']['0'] = [-0.5526]
+    stab_derivs['CZ']['alpha'] = [-5.0676, 5.7736]
+
+    aero_validity = {}
+    # aero_validity['alpha_max_deg'] = 21.7724
+    # aero_validity['alpha_min_deg'] = -7.4485
+    # aero_validity['beta_max_deg'] = 15.
+    # aero_validity['beta_min_deg'] = -15.0
+
+    aero_validity['alpha_max_deg'] = 9.
+    aero_validity['alpha_min_deg'] = -6.
+    aero_validity['beta_max_deg'] = 20.
+    aero_validity['beta_min_deg'] = -20.
+
+
+    return stab_derivs, aero_validity
