638c4b303d6af0b5631f12023bd955dbf2392715
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2021-06-10 20:26:17 +0200

    add soft-kite 3DOF aero model

---
+++ awebox/mdl/aero/kite_dir/three_dof_soft_kite.py
@@ -0,0 +1,162 @@
+#
+#    This file is part of awebox.
+#
+#    awebox -- A modeling and optimization framework for multi-kite AWE systems.
+#    Copyright (C) 2017-2020 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
+#                            ALU Freiburg.
+#    Copyright (C) 2018-2020 Thilo Bronnenmeyer, Kiteswarms Ltd.
+#    Copyright (C) 2016      Elena Malz, Sebastien Gros, Chalmers UT.
+#
+#    awebox is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    awebox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with awebox; if not, write to the Free Software Foundation,
+#    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+'''
+specific aerodynamics for a 3dof soft-kite
+- author: Mark Schelbergen, TU Delft 2021
+- author: Jochem De Schutter, ALU Freiburg 2021
+'''
+
+import casadi.tools as cas
+
+import awebox.tools.vector_operations as vect_op
+import awebox.tools.constraint_operations as cstr_op
+import awebox.tools.print_operations as print_op
+
+import awebox.mdl.aero.kite_dir.frames as frames
+import awebox.mdl.aero.kite_dir.tools as tools
+import awebox.mdl.aero.indicators as indicators
+import awebox.mdl.mdl_constraint as mdl_constraint
+import numpy as np
+
+
+from awebox.logger.logger import Logger as awelogger
+
+
+def get_force_vector(options, variables, atmos, wind, architecture, parameters, kite, outputs):
+    kite_dcm = get_kite_dcm(options, variables, wind, kite, architecture)
+
+    vec_u = tools.get_local_air_velocity_in_earth_frame(options, variables, wind, kite, kite_dcm, architecture,
+                                                        parameters, outputs)
+
+    force_found_frame = 'earth'
+    force_found_vector = get_force_from_u_sym_in_earth_frame(vec_u, options, variables, kite, atmos, wind, architecture,
+                                                             parameters)
+
+    return force_found_vector, force_found_frame, vec_u, kite_dcm
+
+def get_force_cstr(options, variables, atmos, wind, architecture, parameters, outputs):
+
+    cstr_list = mdl_constraint.MdlConstraintList()
+
+    for kite in architecture.kite_nodes:
+        parent = architecture.parent_map[kite]
+
+        force_found_vector, force_found_frame, vec_u, kite_dcm = get_force_vector(options, variables, atmos, wind, architecture, parameters, kite, outputs)
+
+        forces_dict = tools.get_framed_forces(vec_u, kite_dcm, variables, kite, architecture)
+        force_framed_variable = forces_dict[force_found_frame]
+
+        f_scale = tools.get_f_scale(parameters, options)
+
+        resi_f_kite = (force_framed_variable - force_found_vector) / f_scale
+
+        f_kite_cstr = cstr_op.Constraint(expr=resi_f_kite,
+                                        name='f_aero' + str(kite) + str(parent),
+                                        cstr_type='eq')
+        cstr_list.append(f_kite_cstr)
+
+    return cstr_list
+
+def get_force_from_u_sym_in_earth_frame(vec_u, options, variables, kite, atmos, wind, architecture, parameters):
+
+    parent = architecture.parent_map[kite]
+
+    # get relevant variables for kite n
+    q = variables['x']['q' + str(kite) + str(parent)]
+    dq = variables['x']['dq' + str(kite) + str(parent)]
+    yaw = variables['x']['yaw' + str(kite) + str(parent)]
+    pitch = variables['u']['pitch' + str(kite) + str(parent)]
+
+    # wind parameters
+    rho_infty = atmos.get_density(q[2])
+
+    kite_dcm = get_kite_dcm(options, variables, wind, kite, architecture)
+
+    # Basis of body frame expressed in earth frame
+    e1 = kite_dcm[:, 0]  # Longitudinal axis of the wing
+    e2 = kite_dcm[:, 1]  # Transversal axis of the wing
+    e3 = kite_dcm[:, 2]  # Vertical axis of the wing
+
+    kite_projected_area = parameters['theta0', 'geometry', 's_ref']
+    wing_area_side = parameters['theta0', 'geometry', 's_ref_side']
+
+    v_app = - vec_u
+
+    # Flow condition at kite
+    alpha = -cas.dot(e3, v_app) / cas.dot(e1, v_app) - pitch
+    beta = cas.dot(e2, v_app)/cas.dot(e1, v_app)
+
+    lift_coefficient_params = cas.vertcat(
+        parameters['theta0', 'aero', 'CZ', '0'][0],
+        parameters['theta0', 'aero', 'CZ', 'alpha'][0],
+        parameters['theta0', 'aero', 'CZ', 'alpha'][1]
+    )
+
+    drag_coefficient_params = cas.vertcat(
+        parameters['theta0', 'aero', 'CX', '0'][0],
+        parameters['theta0', 'aero', 'CX', 'alpha'][0],
+        parameters['theta0', 'aero', 'CX', 'alpha'][1]
+    )
+
+    c_s = parameters['theta0', 'aero', 'CY', '0'][0]
+
+    # Aerodynamic model
+    c_l = cas.mtimes(lift_coefficient_params.T, cas.vertcat(1, alpha, alpha**2))
+    c_d_kite = cas.mtimes(drag_coefficient_params.T, cas.vertcat(1, alpha, alpha**2))
+    lift = .5*rho_infty*kite_projected_area*cas.norm_2(v_app) * c_l*cas.cross(v_app, e2)  # TODO: note that lift term is
+    # not dependent on v_app^2, since cross(v_app, e2) does not have the same magnitude as v_app
+    drag_kite = -.5*rho_infty*kite_projected_area*cas.norm_2(v_app) * c_d_kite*v_app
+    side_force = .5*rho_infty*wing_area_side*cas.norm_2(v_app)**2 * c_s*beta*e2
+
+    f_aero = lift + drag_kite + side_force
+
+    return f_aero
+
+def get_kite_dcm(options, variables, wind, kite, architecture):
+
+    parent = architecture.parent_map[kite]
+    q = variables['x']['q' + str(kite) + str(parent)]
+    yaw = variables['x']['yaw' + str(kite) + str(parent)]
+
+    if kite == 1:
+
+        l = variables['x']['l_t']
+        l12 = cas.norm_2(q[:2])
+        dcm_tau2e = cas.blockcat([
+            [q[0]*q[2]/(l*l12), -q[1]/l12,  q[0]/l],
+            [q[1]*q[2]/(l*l12), q[0]/l12, q[1]/l],
+            [-l12/l,              0,         q[2]/l],
+        ])  # Transformation from tangential (to unit sphere surface) plane to earth frame
+        dcm_b2tau = cas.blockcat([
+            [cas.cos(yaw), -cas.sin(yaw), 0],
+            [cas.sin(yaw), cas.cos(yaw),  0],
+            [0,  0,   1],
+        ])
+        kite_dcm = cas.mtimes(dcm_tau2e, dcm_b2tau) 
+
+    elif kite > 1:
+        raise ValueError('Soft-kite model is only implemented for single-kite systems.')
+
+    return kite_dcm
