be7eabd0bf264ad87e6a24d49a2a951fd39fef07
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2021-06-26 11:21:19 +0200

    update soft-kite model draft

--- examples/soft_kite_model.py
+++ examples/soft_kite_model.py
@@ -5,46 +5,65 @@ import matplotlib.pyplot as plt
 import numpy as np
 import casadi as ca
 
-# single kite with point-mass model
+# single pumping soft-kite with kitepower point-mass model
 options = {}
-options['user_options.trajectory.type'] = 'power_cycle'
-options['user_options.trajectory.system_type'] = 'lift_mode'
-options['user_options.trajectory.lift_mode.windings'] = 1
-
 options['user_options.system_model.architecture'] = {1:0}
 options['user_options.system_model.kite_dof'] = 3
 options['user_options.system_model.kite_type'] = 'soft'
+options['user_options.trajectory.system_type'] = 'lift_mode'
+options['user_options.kite_standard'] = awe.kitepower_data.data_dict()
+
+# other model options
 options['model.tether.use_wound_tether'] = False
 options['user_options.induction_model'] = 'not_in_use'
-options['user_options.kite_standard'] = awe.kitepower_data.data_dict()
-options['params.tether.rho'] = 724.0
-options['params.tether.cd'] = 1.1
-options['model.tether.control_var'] = 'dddl_t'
-options['model.system_bounds.x.ddl_t'] = [-2.0, 2.0]
-options['model.system_bounds.x.dl_t'] = [-10.0, 10.0]
-options['model.system_bounds.u.pitch'] = [0.0, np.pi/6]
-options['model.system_bounds.u.dyaw'] = [-1.5, 1.5]
+options['params.tether.rho'] = 724.0 # kg/m^3
+options['params.tether.cd'] = 1.1 
+options['model.tether.control_var'] = 'dddl_t' # tether jerk control
+options['user_options.wind.model'] = 'log_wind'
+options['user_options.wind.u_ref'] = 9.0
 
+# system bounds
+options['model.system_bounds.x.ddl_t'] = [-2.0, 2.0] # m/s^2
+options['model.system_bounds.x.dl_t'] = [-10.0, 10.0] # m/s
+options['model.system_bounds.u.pitch'] = [0.0, np.pi/6] # rad
+options['model.system_bounds.u.dyaw'] = [-5., 5.] # rad/s
+options['model.system_bounds.x.q'] = [
+    np.array([-ca.inf, -ca.inf, 10.0]), # z > 10 m
+    np.array([ca.inf, ca.inf, ca.inf])
+    ] 
+
+# model bounds
+options['model.model_bounds.tether_stress.include']= True
+options['params.tether.f_max'] = 3.0 # tether stress safety factor
 options['model.model_bounds.acceleration.include'] = False
-options['nlp.n_k'] = 20
-options['solver.initialization.groundspeed'] = 15.
+options['model.model_bounds.aero_validity.include'] = True # bounds on alpha, beta
+options['model.model_bounds.tether_force.include'] = False
+options['params.model_bounds.tether_force_limits'] = np.array([1e3, 5e3])
+
+# trajectory options
+options['user_options.trajectory.type'] = 'power_cycle'
+options['user_options.trajectory.lift_mode.windings'] = 5
+
+# NLP options
+options['nlp.n_k'] = 40
+options['nlp.collocation.u_param'] = 'poly'
+
+# initialization
+options['solver.initialization.shape'] = 'lemniscate'
+options['solver.initialization.groundspeed'] = 30.
 options['solver.initialization.winding_period'] = 30.
 options['solver.initialization.theta.diam_t'] = 5e-3
 options['solver.initialization.l_t'] = 200.0
-options['params.tether.f_max'] = 3.0
-options['solver.initialization.shape'] = 'lemniscate'
-options['visualization.cosmetics.plot_ref'] = True
+
+# visualization options
+options['visualization.cosmetics.plot_ref'] = False
 options['visualization.cosmetics.plot_bounds'] = True
-options['user_options.wind.model'] = 'uniform'
-options['user_options.wind.u_ref'] = 5.0
-options['solver.weights.yaw'] = 1.0
-options['model.model_bounds.aero_validity.include'] = True
 
 # optimize trial
 trial = awe.Trial(options, 'single_kite_lift_mode')
 trial.build()
-trial.optimize(final_homotopy_step='initial')
-trial.plot(['constraints','states', 'controls'])
+trial.optimize(final_homotopy_step='final')
+trial.plot(['states', 'controls', 'quad'])
 
 outputs = trial.visualization.plot_dict['outputs']
 aero_out = outputs['aerodynamics']
@@ -94,4 +113,8 @@ for k in range(len(aero_out['ehat_chord1'][0])):
     ortho.append(ca.mtimes(dcm.T, dcm) - np.eye(3))
     v_app.append(ua)
 
+plt.figure()
+plt.plot(aero_out['airspeed1'][0])
+plt.grid(True)
+plt.title('airspeed [m/s]')
 plt.show()
