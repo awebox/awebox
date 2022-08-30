bbb1b8f3cf137beacb67b92b856e88dffa3c31d0
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2022-08-30 22:45:11 +0200

    ideal tracking example

--- examples/mpc_closed_loop.py
+++ examples/mpc_closed_loop.py
@@ -6,8 +6,9 @@ MPC-based closed loop simulation example for a single 3DOF kite system.
 # imports
 import awebox as awe
 import casadi as ca
-import pickle
 import copy
+import matplotlib.pyplot as plt
+
 from awebox.logger.logger import Logger as awelogger
 awelogger.logger.setLevel('DEBUG')
 
@@ -21,7 +22,7 @@ options['user_options.system_model.kite_dof'] = 3
 options['user_options.trajectory.type'] = 'power_cycle'
 options['user_options.trajectory.system_type'] = 'lift_mode'
 options['user_options.trajectory.lift_mode.windings'] = 1
-
+options['model.system_bounds.theta.t_f'] =  [5.0, 15.0]
 # wind model
 options['params.wind.z_ref'] = 10.0
 options['user_options.wind.model'] = 'log_wind'
@@ -30,7 +31,7 @@ options['user_options.wind.u_ref'] = 5.
 # NLP discretization
 options['nlp.n_k'] = 40
 options['nlp.collocation.u_param'] = 'zoh'
-options['user_options.trajectory.lift_mode.phase_fix'] = 'single_reelout'
+options['user_options.trajectory.lift_mode.phase_fix'] = 'simple'
 options['solver.linear_solver'] = 'ma57' # if HSL
 options['solver.mu_hippo'] = 1e-2
 
@@ -38,10 +39,12 @@ options['solver.mu_hippo'] = 1e-2
 trial = awe.Trial(options, 'single_kite_lift_mode')
 trial.build()
 trial.optimize()
+trial.plot(['isometric'])
+plt.show()
 
 # set-up closed-loop simulation
-N_mpc = 15 # MPC horizon
-N_sim = 1000  # closed-loop simulation steps
+N_mpc = 10 # MPC horizon
+N_sim = 200  # closed-loop simulation steps
 ts = 0.1 # sampling time
 
 # MPC options
@@ -65,11 +68,10 @@ options['sim.number_of_finite_elements'] = 20 # integrator steps within one samp
 options['sim.sys_params'] = copy.deepcopy(trial.options['solver']['initialization']['sys_params_num'])
 
 # reduce average wind speed
-options['sim.sys_params']['wind']['u_ref'] = 0.9*options['sim.sys_params']['wind']['u_ref']
+options['sim.sys_params']['wind']['u_ref'] = 1.0*options['sim.sys_params']['wind']['u_ref']
 
 # make simulator
 closed_loop_sim = awe.sim.Simulation(trial, 'closed_loop', ts, options)
 closed_loop_sim.run(N_sim)
-closed_loop_sim.plot(['states','controls','algebraic_variables','constraints','invariants','quad'])
-import matplotlib.pyplot as plt
+closed_loop_sim.plot(['isometric','states'])
 plt.show()
