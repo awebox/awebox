55276dc245a028fca7fb47d38cd67f5ce7277734
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2024-10-22 10:56:13 +0200

    switch off beta cost for speed in ampyx ap2 particular example

--- examples/ampyx_ap2_trajectory.py
+++ examples/ampyx_ap2_trajectory.py
@@ -47,10 +47,11 @@ def run(plot_show_block=True, overwrite_options={}):
     options['nlp.collocation.u_param'] = 'zoh'
     options['user_options.trajectory.lift_mode.phase_fix'] = 'simple' # 'single_reelout'
     options['solver.linear_solver'] = 'ma57'  # if HSL is installed, otherwise 'mumps'
+    options['nlp.cost.beta'] = False # penalize side-slip (can improve convergence)
 
     # (experimental) set to "True" to significantly (factor 5 to 10) decrease construction time
     # note: this may result in slightly slower solution timings
-    options['nlp.compile_subfunctions'] = False
+    options['nlp.compile_subfunctions'] = True
 
     for option_name, option_val in overwrite_options.items():
         options[option_name] = option_val
