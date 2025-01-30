59555517949eda7853d0e2df648c88e045bc1c81
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2025-01-30 15:45:36 +0100

    add three_dof_kite_data optionsf ile

---
+++ awebox/opts/kite_data/three_dof_kite_data.py
@@ -0,0 +1,77 @@
+#
+#    This file is part of awebox.
+#
+#    awebox -- A modeling and optimization framework for multi-kite AWE systems.
+#    Copyright (C) 2017-2020 Jochem De Schutter, Rachel Leuthold, Moritz Diehl,
+#                            ALU Freiburg.
+#    Copyright (C) 2018-2020 Thilo Bronnenmeyer, Kiteswarms Ltd.
+#    Copyright (C) 2016      Elena Malz, Sebastien Gros, Chalmers UT.
+#
+#    awebox is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    awebox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with awebox; if not, write to the Free Software Foundation,
+#    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+import numpy as np
+from casadi.tools import vertcat
+
+def data_dict():
+
+    data_dict = {}
+    data_dict['name'] = 'ampyx'
+
+    data_dict['geometry'] = geometry() # kite geometry
+
+    stab_derivs, aero_validity = aero()
+    data_dict['stab_derivs'] = stab_derivs # stability derivatives
+    data_dict['aero_validity'] = aero_validity
+
+    return data_dict
+
+def geometry():
+
+    geometry = {}
+    # 'aerodynamic parameter identification for an airborne wind energy pumping system', licitra, williams, gillis, ghandchi, sierbling, ruiterkamp, diehl, 2017
+    # 'numerical optimal trajectory for system in pumping mode described by differential algebraic equation (focus on ap2)' licitra, 2014
+    geometry['s_ref'] = 200  # [m^2]
+    geometry['ar'] = 10.0
+
+    geometry['b_ref'] = np.sqrt(geometry['ar'] * geometry['s_ref'])  # [m]
+    geometry['c_ref'] = geometry['s_ref'] / geometry['b_ref']  # [m]
+    geometry['m_k'] = 20 * geometry['s_ref']  # [kg]
+
+    # tether attachment point
+    geometry['r_tether'] = np.zeros((3,1))
+
+    return geometry
+
+def aero():
+
+    stab_derivs = {}
+
+    stab_derivs['frame'] = {}
+    stab_derivs['frame']['force'] = 'control'
+    stab_derivs['frame']['moment'] = 'control'
+
+    stab_derivs['CD'] = {}
+    stab_derivs['CD']['0'] = [0.02]
+    stab_derivs['CD']['alpha'] = [0]
+
+    aero_validity = {}
+    aero_validity['alpha_max_deg'] = 20.0
+    aero_validity['alpha_min_deg'] = 0.0
+    aero_validity['beta_max_deg'] = -20.0
+    aero_validity['beta_min_deg'] = 20.0
+
+
+    return stab_derivs, aero_validity
