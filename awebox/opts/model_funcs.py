[ALL TESTS RUN except size trials and actuator trials] forgot to pull over the scaling fix from the other branch that makes the convergence tests match the paper plots. here it is.

awebox/opts/model_funcs.py
@@ -1054,9 +1054,7 @@ def build_fict_scaling_options(options, options_tree, fixed_params, architecture
     length = options['solver']['initialization']['l_t']
     tension = tension_per_unit_length * length
 
-    available_estimates = [tension, gravity_force, centripetal_force, aero_force]
-    if options['model']['model_bounds']['acceleration']['include']:
-        available_estimates += [max_acceleration_force]
+    available_estimates = [max_acceleration_force, tension, gravity_force, centripetal_force, aero_force]
     synthesized_force = vect_op.synthesize_estimate_from_a_list_of_positive_scalar_floats(available_estimates)
 
     force_scaling_dict = {'max_acceleration': max_acceleration_force,
@@ -1243,11 +1241,7 @@ def estimate_flight_radius(options, architecture, suppress_help_statement=False)
         length = options['solver']['initialization']['theta']['l_s']
     cone_radius = float(length * np.sin(cone_angle))
 
-    available_estimates = [cone_radius]
-    if options['model']['model_bounds']['anticollision']['include']:
-        available_estimates += [anticollision_radius]
-    if options['model']['model_bounds']['acceleration']['include']:
-        available_estimates += [centripetal_radius]
+    available_estimates = [anticollision_radius, centripetal_radius, cone_radius]
     synthesized_radius = vect_op.synthesize_estimate_from_a_list_of_positive_scalar_floats(available_estimates)
 
     radius_dict = {'anticollision': anticollision_radius,
@@ -1463,11 +1457,7 @@ def estimate_main_tether_tension_per_unit_length(options, architecture, suppress
     tension_estimate_via_max_force = options['params']['model_bounds']['tether_force_limits'][1]
     tension_estimate_via_average_force = (tension_estimate_via_min_force + tension_estimate_via_max_force)/2.
 
-    available_estimates = [tension_estimate_via_power, tension_estimate_via_force_summation]
-    if options['model']['model_bounds']['tether_stress']['include']:
-        available_estimates += [tension_estimate_via_max_stress]
-    elif options['model']['model_bounds']['tether_force']['include']:
-        available_estimates += [tension_estimate_via_max_force]
+    available_estimates = [tension_estimate_via_power, tension_estimate_via_max_stress, tension_estimate_via_average_force, tension_estimate_via_force_summation]
     tension_estimate_via_synthesis = vect_op.synthesize_estimate_from_a_list_of_positive_scalar_floats(available_estimates)
 
     tension_estimate_dict = {'power': tension_estimate_via_power,