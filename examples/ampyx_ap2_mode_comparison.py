309ce3180b53a9f7f0ca5b2600a19721ea7b4a0d
abavoil <17646791+abavoil@users.noreply.github.com>
2026-03-25 19:48:34 +0100

    lift not converging: high residuals for unlimited airspace, 2 superimposed paths on reduced airspace

--- examples/ampyx_ap2_mode_comparison.py
+++ examples/ampyx_ap2_mode_comparison.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
+'''
 Circular pumping trajectory for the Ampyx AP2 aircraft.
 Model and constraints as in:
 
-"Performance assessment of a rigid wing Airborne Wind Energy pumping system",
+'Performance assessment of a rigid wing Airborne Wind Energy pumping system',
 G. Licitra, J. Koenemann, A. Bürger, P. Williams, R. Ruiterkamp, M. Diehl
 Energy, Vol.173, pp. 569-585, 2019.
 
 :author: Jochem De Schutter
 :edited: Rachel Leuthold
-"""
+'''
 
+import shutil
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,10 +19,18 @@ import csv
 from datetime import datetime
 from copy import deepcopy
 import time
+import pickle
+
+import multiprocess.context as ctx
+from pathos.multiprocessing import ProcessPool as Pool
+import platform
 
 import awebox as awe
 import awebox.opts.kite_data.ampyx_ap2_settings as ampyx_ap2_settings
 import awebox.tools.print_operations as print_op
+
+MULTIPROCESSING = True
+DUMMY_RUN = False
 
 # Founf in Trial.print_solution()
 THETA_INFO = {
@@ -76,6 +85,7 @@ def common_options():
     options['params.model_bounds.tether_force_limits'] = np.array([1e0, 7.5e3])
     options['model.model_bounds.tether_stress.include'] = True
     options['model.system_bounds.x.q'] = np.array([-np.inf, -np.inf, 10]), np.array([np.inf, np.inf, np.inf])
+    # options['model.system_bounds.x.l_t'] = [1.0e-2, 120.0]
 
     # Initialization
     options['solver.initialization.groundspeed'] = 55  # m/s
@@ -83,14 +93,14 @@ def common_options():
 
     # NLP options
     # By default, direct collocation using Radau scheme with order 4 lagrange polynomials
-    options['nlp.n_k'] = 80
+    options['nlp.n_k'] = 40  # refine this value for longer periods
     options['nlp.collocation.u_param'] = 'zoh'
     options['user_options.trajectory.lift_mode.phase_fix'] = 'simple' # 'single_reelout'
     options['solver.linear_solver'] = 'ma57'  # if HSL is installed, otherwise 'mumps'
     options['nlp.cost.beta'] = False # penalize side-slip (can improve convergence)
     options['solver.cost.theta_regularisation.0'] = 1e-8  # Default of 1 barely optimizes the parameters
 
-    # (experimental) set to "True" to significantly (factor 5 to 10) decrease construction time
+    # (experimental) set to 'True' to significantly (factor 5 to 10) decrease construction time
     # note: this may result in slightly slower solution timings
     options['nlp.compile_subfunctions'] = False
 
@@ -99,22 +109,22 @@ def common_options():
 
 def drag_mode_options(options):
     options['user_options.trajectory.system_type'] = 'drag_mode'
-    options['model.system_bounds.theta.t_f'] = [3, 15]  # more than 25 seconds allows for ill solutions
-    options['nlp.n_k'] = 20
+    options['model.system_bounds.theta.t_f'] = [4, 15]  # more than 25 seconds allows for ill solutions
+    options['nlp.n_k'] = 40
     return options
 
 def lift_mode_options(options):
     options['user_options.trajectory.system_type'] = 'lift_mode'
-    options['user_options.trajectory.lift_mode.windings'] = 2
-    options['model.system_bounds.theta.t_f'] = [30, 80]
+    options['user_options.trajectory.lift_mode.windings'] = 4
+    options['model.system_bounds.theta.t_f'] = [20, 80]  # 80 seconds: 2 pumps
     options['nlp.n_k'] = 80
 
     return options
 
-def rocking_mode_options(options):
+def base_rocking_mode_options(options):
     options['user_options.trajectory.system_type'] = 'rocking_mode'
     options['model.system_bounds.theta.t_f'] = [2, 8]
-    options['nlp.n_k'] = 10
+    options['nlp.n_k'] = 40
 
     ## Rocking mode options
     # Parameter values
@@ -140,21 +150,22 @@ def rocking_mode_options(options):
     options['solver.initialization.inclination_deg'] = 30
     options['solver.initialization.lemniscate.az_width_deg'] = 60
     options['solver.initialization.lemniscate.el_width_deg'] = 20
-    options['solver.initialization.lemniscate.rise_on_sides'] = False
+    options['solver.initialization.lemniscate.rise_on_sides'] = True
 
     return options
 
 
-"""
+'''
 example 0: Finding the optimal kite trajectory for fixed arm parameters and no arm control
 No parametric optimization, no control, lines of 35 m. Optimal control of the kite.
-"""
+'''
 def rocking_mode_example_0(options):
+    options = base_rocking_mode_options(options)
     return options
 
-"""
+'''
 example 1: example 0 + parametric optimization
-"""
+'''
 def rocking_mode_example_1(options):
     options = rocking_mode_example_0(options)
     options['solver.initialization.l_t'] = None
@@ -162,29 +173,29 @@ def rocking_mode_example_1(options):
     options['solver.initialization.theta.torque_slope'] = None
     return options
 
-"""
+'''
 example 2: example 1 + optimal control of the arm with constraints
-"""
+'''
 def rocking_mode_example_2(options):
     rocking_mode_example_1(options)
     options['user_options.trajectory.rocking_mode.enable_arm_control'] = True
     options['model.system_bounds.u.dactive_torque'] = [-1000, 1000]
     return options
 
-"""
+'''
 example 3: example 2 + no constraints on arm control
 Note: there are still constraints on the tether tension which indirectly act on the control of the arm
 
 Solution is not sound, energy balance is off
-"""
+'''
 def rocking_mode_example_3(options):
     rocking_mode_example_2(options)
     options.pop('model.system_bounds.u.dactive_torque')
     return options
 
-"""
+'''
 example 1 but the arm inertia and passive torque are entirely replaced by controlled torque
-"""
+'''
 def rocking_mode_example_4(options):
     options['user_options.trajectory.rocking_mode.enable_arm_control'] = True
     options['user_options.trajectory.fixed_params']['arm_inertia'] = 100
@@ -192,9 +203,40 @@ def rocking_mode_example_4(options):
     options['model.system_bounds.x.active_torque'] = [-1000, 1000]
     return options
 
+def reduced_airspace(options, halfsize, zmin):
+    mode = options['user_options.trajectory.system_type']
+    if mode == 'lift_mode':
+        options['model.system_bounds.theta.t_f'] = [5, 15]  # more than 25 seconds allows for ill solutions
+        options['user_options.trajectory.lift_mode.windings'] = 2
+    elif mode == 'drag_mode':
+        options['model.system_bounds.theta.t_f'] = [2, 5]
+    elif mode == 'rocking_mode':
+        options['model.system_bounds.theta.t_f'] = [2, 5]
+    
+    options['solver.initialization.l_t'] = 50.0
+    options['model.system_bounds.x.l_t'] = [5.0, 350.0]  # Halved
+    options['model.system_bounds.x.dl_t'] = [-30.0, 40.0]  # Doubled
+    options['model.system_bounds.x.ddl_t'] = [-4.8, 4.8]  # Doubled
+
+    options['model.system_bounds.x.q'] = np.array([-np.inf, -halfsize, zmin]), np.array([halfsize, halfsize, halfsize])
+    return options
+
+def alter_arm_length(epsilon, options):
+    options['user_options.trajectory.fixed_params']['arm_length'] += epsilon
+    return options
+
+def rocking_mode_options(options):
+    return rocking_mode_example_2(options)
+
+def free_arm_rocking_mode_options(options):
+    options = rocking_mode_options(options)
+    options['solver.initialization.theta.arm_length'] = None
+    options['model.system_bounds.theta.arm_length'] = [1e-1, 1e3]
+    return options
+
 ## Running and processing the experiments
 
-"""
+'''
 If initialization is set for any parameter (solver.initialization.l_t or solver.initialization.theta.***),
 remove it from user_options.trajectory.fixed_params
 Initializing with None, as in `options['solver.initialization.theta.arm_inertia'] = None`, will use the value found in `fixed_params`
@@ -203,7 +245,7 @@ Initializing with None, as in `options['solver.initialization.theta.arm_inertia'
  - don't set `solver.initialization.***` -> same value in fixed_params and `solver.initialization.***`.
  - set `solver.initialization.***` to None -> popped value from fixed_params and assign it to `solver.initialization.***` instead of None
  - set  `solver.initialization.***` to a value -> popped value in fixed_params, and `solver.initialization.***` stays untouched
-"""
+'''
 def post_process_options_for_parameter_optimization(options):
     fixed_params = options['user_options.trajectory.fixed_params']
 
@@ -286,68 +328,95 @@ def print_stats(plot_dict):
     _print_stats(u_kite, 'Kite speed (m/s)')
 
 def save_all_figures(directory, prefix):
-    """Saves and closes all open matplotlib figures."""
+    '''Saves and closes all open matplotlib figures.'''
     figs = [plt.figure(n) for n in plt.get_fignums()]
     for i, fig in enumerate(figs):
-        filename = f"{prefix}_plot_{i}.png"
+        filename = f'{prefix}_plot_{i}.png'
         path = os.path.join(directory, filename)
         fig.savefig(path)
-        print_op.base_print(f"Saved plot to: {path}", level='info')
+        print_op.base_print(f'Saved plot to: {path}', level='info')
     plt.close('all')
 
 def get_step_results(trial, step_name):
-    """Extracts quantities from the trial object similar to print_solution."""
+    '''Extracts quantities from the trial object similar to print_solution.'''
     opt = trial.optimization
-    res = {"Step": step_name}
+    res = {'Step': step_name}
 
     # Time period - Cast CasADi DM to float before rounding
     t_f = float(opt.global_outputs_opt['time_period'])
-    res["Time period (s)"] = round(t_f, 3)
+    res['Time period (s)'] = round(t_f, 3)
 
     # Average Power
     if 'e' in trial.model.integral_outputs.keys():
         e_final = float(opt.integral_outputs_final_si['int_out', -1, 'e'])
     else:
         e_final = float(opt.V_final_si['x', -1, 'e'][-1])
-    res["Average power output (kW)"] = round((e_final / t_f) / 1000.0, 4)
+    res['Average power output (kW)'] = round((e_final / t_f) / 1000.0, 4)
 
     # Theta Parameters
     for theta in trial.model.variables_dict['theta'].keys():
         if theta != 't_f' and theta in THETA_INFO:
             info = THETA_INFO[theta]
             val = float(opt.V_final_si['theta', theta])
-            res[f"{info[0]} ({info[2]})"] = round(val * info[1], 4)
+            res[f'{info[0]} ({info[2]})'] = round(val * info[1], 4)
+    
+    plot_dict = trial.visualization.plot_dict
+    z = plot_dict['x']['q10'][2]
+    u_wind = awe.opts.model_funcs.get_u_at_altitude(plot_dict['options'], z)
+    u_kite = np.linalg.norm(np.array(plot_dict['x']['dq10']), axis=0)
+
+    
+    res['mean(wind^3)^(1/3) (m/s)'] = np.cbrt(np.mean(u_wind**3))
+    res['Median wind speed (m/s)'] = np.median(u_wind)
+    res['Median kite speed (m/s)'] = np.median(u_kite)
+    res['q residual (m)'] = np.max(np.abs(plot_dict['outputs']['invariants']['c10']))
+    res['dq residual (m/s)'] = np.max(np.abs(plot_dict['outputs']['invariants']['dc10']))
+    res['ddq residual(m/s^2)'] = np.max(np.abs(plot_dict['outputs']['invariants']['ddc10']))
     
     return res
 
-def run_task(task_idx, total_tasks, subfolder, prefix, config_func, base_options, base_path):
-    header = f" [{task_idx}/{total_tasks}] STARTING TASK: {prefix} "
-    print("\n" + "="*80)
-    print(header.center(80, "="))
-    print("="*80 + "\n")
+def run_task(subfolder, prefix, config_func, base_options, base_path):
+    shutil.rmtree('cache', ignore_errors=True)
+    
+    header = f' STARTING TASK: {prefix} '
+    print('\n' + '='*80)
+    print(header.center(80, '='))
+    print('='*80 + '\n')
 
     start_time = time.time()
     save_dir = os.path.join(base_path, subfolder)
     os.makedirs(save_dir, exist_ok=True)
-    csv_path = os.path.join(save_dir, f"{prefix}_results.csv")
+    csv_path = os.path.join(save_dir, f'{prefix}_results.csv')
+    seed_path = os.path.join(save_dir, f'{prefix}_seed.pickle')
+    options_path = os.path.join(save_dir, f'{prefix}_options.pickle')
     
     csv_data = []
     try:
         options = config_func(deepcopy(base_options))
         options = post_process_options_for_parameter_optimization(options)
+        
+        if DUMMY_RUN:
+            options['nlp.n_k'] = 2
+
         trial = awe.Trial(options, prefix)
         trial.build()
+
+        with open(seed_path, 'wb') as f:
+            pickle.dump(trial.options_seed, f)
+
+        with open(options_path, 'wb') as f:
+            pickle.dump(trial.options, f)
         
         steps = ['initial_guess', 'initial', 'fictitious', 'power', 'final']
         for step in steps:
-            print_op.base_print(f"Homotopy Step: {step}", level='info')
+            print_op.base_print(f'Homotopy Step: {step}', level='info')
             trial.optimize(final_homotopy_step=step)
             
             print_stats(trial.visualization.plot_dict)
             csv_data.append(get_step_results(trial, step))
 
-            trial.plot(['states', 'quad'])
-            save_all_figures(save_dir, f"{prefix}_step_{step}")
+            trial.plot(['states', 'quad', 'invariants'])
+            save_all_figures(save_dir, f'{prefix}_step_{step}')
 
         if csv_data:
             keys = csv_data[0].keys()
@@ -355,39 +424,73 @@ def run_task(task_idx, total_tasks, subfolder, prefix, config_func, base_options
                 dict_writer = csv.DictWriter(f, fieldnames=keys)
                 dict_writer.writeheader()
                 dict_writer.writerows(csv_data)
-            print_op.base_print(f"Results saved to {csv_path}", level='info')
+            print_op.base_print(f'Results saved to {csv_path}', level='info')
 
         # Timing Calculation
         duration = time.time() - start_time
         
         # Visible Footer
-        footer = f" TASK {prefix} COMPLETED IN {duration:.2f}s "
-        print("\n" + "-"*80)
-        print(footer.center(80, "-"))
-        print("-"*80)
+        footer = f' TASK {prefix} COMPLETED IN {duration:.2f}s '
+        print('\n' + '-'*80)
+        print(footer.center(80, '-'))
+        print('-'*80)
+        print('\n' * 3)
 
     except Exception as e:
         end_time = time.time()
-        print_op.base_print(f"CRITICAL ERROR in task {prefix} after {end_time-start_time:.2f}s: {str(e)}", level='error')
+        print_op.base_print(f'CRITICAL ERROR in task {prefix} after {end_time-start_time:.2f}s: {str(e)}', level='error')
         raise(e)
 
 def main():
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    base_path = os.path.join("outputs", timestamp)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    base_path = os.path.join('outputs', timestamp)
     
-    # Define tasks: (Subfolder, Filename Prefix, Function)
+    # Define tasks: (Subfolder, Filename_Prefix, Function)
     tasks = [
-        ("lift", "lift_mode", lambda opt: lift_mode_options(opt)),
-        ("drag", "drag_mode", lambda opt: drag_mode_options(opt)),
-        ("rocking", "rocking_ex1", lambda opt: rocking_mode_example_1(rocking_mode_options(opt))),
-        ("rocking", "rocking_ex2", lambda opt: rocking_mode_example_2(rocking_mode_options(opt))),
+        ('exp1', 'lift', lambda opt: lift_mode_options(opt)),
+        ('exp1', 'drag', lambda opt: drag_mode_options(opt)),
+        ('exp1', 'rocking', lambda opt: rocking_mode_example_2(opt)),
+        # ('exp1', 'rocking_opt_arm', lambda opt: free_arm_rocking_mode_options(opt)),
+        # ('exp2_diff-div', 'm1', lambda opt: alter_arm_length(-1e-1, rocking_mode_example_2(opt))),  # a - 1e-1
+        # ('exp2_diff-div', 'm2', lambda opt: alter_arm_length(-1e-2, rocking_mode_example_2(opt))),  # a - 1e-2
+        # ('exp2_diff-div', 'm3', lambda opt: alter_arm_length(-1e-3, rocking_mode_example_2(opt))),  # a - 1e-3
+        # ('exp2_diff-div', 'p1', lambda opt: alter_arm_length(+1e-1, rocking_mode_example_2(opt))),  # a + 1e-1
+        # ('exp2_diff-div', 'p2', lambda opt: alter_arm_length(+1e-2, rocking_mode_example_2(opt))),  # a + 1e-2
+        # ('exp2_diff-div', 'p3', lambda opt: alter_arm_length(+1e-3, rocking_mode_example_2(opt))),  # a + 1e-3
+        ('exp3_reduced_airspace', 'lift', lambda opt: reduced_airspace(lift_mode_options(opt), halfsize=50, zmin=5)),
+        ('exp3_reduced_airspace', 'drag', lambda opt: reduced_airspace(drag_mode_options(opt), halfsize=50, zmin=5)),
+        ('exp3_reduced_airspace', 'rocking', lambda opt: reduced_airspace(rocking_mode_example_2(opt), halfsize=50, zmin=5)),
+        # ('exp3_reduced_airspace', 'rocking_opt_arm', lambda opt: reduced_airspace(free_arm_rocking_mode_options(opt), halfsize=50, zmin=5)),
     ]
     
     base_options = common_options()
     
-    for i, (subfolder, prefix, func) in enumerate(tasks, 1):
-        run_task(i, len(tasks), subfolder, prefix, func, base_options, base_path)
-        print("\n" * 3)
+    if MULTIPROCESSING:
+        full_args = [t + (base_options, base_path) for t in tasks]
+        run_task_wrapper = lambda args: run_task(*args, base_options, base_path)
+        with Pool() as pool:
+            pool.map(run_task_wrapper, tasks)
+    else:
+        for subfolder, prefix, func in tasks:
+            run_task(subfolder, prefix, func, base_options, base_path)
 
-if __name__ == "__main__":
+
+def print_pickled_params(path):
+    with open(path, "rb") as f:
+        d = pickle.load(f)
+    
+    print(*d, sep="\n")
+    return d
+
+if __name__ == '__main__':
+    t0 = time.time()
+    if MULTIPROCESSING and platform.system() == "Darwin":
+        ctx._force_start_method('spawn')
     main()
+    print("Total time: ", time.time() - t0)
+
+# Modèles comparables car rendements 100%
+# Romain: cycle limite avec le kite qui pousse le bras
+# Modifier la statistique vent moyen pour intégrer puissance au cube et avoir une puissance disponible moyenne
+# augmenter longueur du bras jusqu'à avoir une puissance disponible moyenne du même ordre que les autres
+# Contrôle de ddα directement
